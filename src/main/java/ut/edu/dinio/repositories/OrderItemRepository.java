192910eadd55cc2000617fe6adb4305a297287e0
HieuShin7703 <coccoccoc2005@gmail.com>
2026-01-29 01:10:42 +0700

    feat(kitchen): triển khai hệ thống quản lý trạng thái món ăn (KDS) thời gian thực và quản lý lịch sử món ăn

--- src/main/java/ut/edu/dinio/repositories/OrderItemRepository.java
+++ src/main/java/ut/edu/dinio/repositories/OrderItemRepository.java
@@ -2,11 +2,24 @@ package ut.edu.dinio.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import ut.edu.dinio.pojo.OrderItem;
+import ut.edu.dinio.pojo.enums.OrderItemStatus;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
   List<OrderItem> findByOrderIdOrderByIdAsc(Integer orderId);
   List<OrderItem> findByOrderId(Integer orderId);
+
+  @Query("SELECT oi FROM OrderItem oi " +
+            "JOIN FETCH oi.order o " +
+            "JOIN FETCH o.session s " +
+            "JOIN FETCH s.table t " +
+            "JOIN FETCH oi.menuItem mi " +
+            "JOIN FETCH mi.category c " +
+            "WHERE oi.status IN :statuses " +
+            "ORDER BY o.createdAt ASC")
+    List<OrderItem> findKitchenItems(@Param("statuses") List<OrderItemStatus> statuses);
 }
